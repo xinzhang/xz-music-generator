7afd0b42f262278652f8e7c985b67e321395e7a3
xinzhang <xin_zhang_2000@yahoo.com>
2025-08-09 21:02:51 +1000

    add home/create

---
+++ frontend/src/app/(main)/create/page.tsx
@@ -0,0 +1,21 @@
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import CreateSong from "~/components/create";
+import { SongPanel } from "~/components/create/song-panel";
+import { auth } from "~/lib/auth";
+
+export default async function HomePage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),    
+  })
+  if (!session) {
+    redirect("/auth/sign-in")
+  }
+  
+  return (
+
+    <div className="flex h-full flex-col lg:flex-row">
+      <SongPanel />
+    </div>
+  );
+}
